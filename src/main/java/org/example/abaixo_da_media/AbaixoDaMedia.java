1f619cd3f9627c361b1c2d5f923b8d97de7bace9
Bernard De Freitas <bernard30101994@gmail.com>
2025-06-10 12:26:37 -0300

    adicionar lógica para calcular e exibir elementos abaixo da média em AbaixoDaMedia.java

--- src/main/java/org/example/abaixo_da_media/AbaixoDaMedia.java
+++ src/main/java/org/example/abaixo_da_media/AbaixoDaMedia.java
@@ -1,4 +1,35 @@
 package org.example.abaixo_da_media;
 
+import java.util.Scanner;
+
 public class AbaixoDaMedia {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quantos elementos vai ter o vetor?");
+        int limiteVetor = scanner.nextInt();
+
+        double[] vetor = new double[limiteVetor];
+        double soma = 0;
+
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("Digite um numero: ");
+            vetor[i] = scanner.nextDouble();
+            soma += vetor[i];
+        }
+        double media = soma / vetor.length;
+
+        System.out.printf("MÉDIA DO VETOR = %.3f" , media);
+        System.out.println();
+
+        System.out.println("ELEMENTOS ABAIXO DA MEDIA:");
+        for (int i = 0; i < vetor.length; i++) {
+            if(vetor[i] < media) {
+                System.out.printf("%.1f\n", vetor[i]);
+            }
+        }
+
+    }
 }
